c7c32b8a96febb064f25f53d72d1e26e32bf3a82
arya020595 <aryarifqipratama@gmail.com>
2026-05-18 15:08:21 +0700

    feat(config): add staging environment configuration

--- config/config.js
+++ config/config.js
@@ -40,4 +40,17 @@ module.exports = {
     seederStorage: "sequelize",
     seederStorageTableName: "SequelizeData",
   },
+  staging: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+    logging: false,
+    migrationStorage: "sequelize",
+    migrationStorageTableName: "SequelizeMeta",
+    seederStorage: "sequelize",
+    seederStorageTableName: "SequelizeData",
+  },
 };
