7ade455e51ae24b070bbeb5b2f0bda2e36806ee1
Fadzyll <fadzzf8@gmail.com>
2026-05-22 17:45:51 +0800

    Add explore_sabah asset and update PDF generators
    Add explore_sabah-without_bg.png to the assets folder and update all PDF generator utilities to load the image from ../assets instead of the old frontend path. This makes the image available to the backend PDF generators (booking, accommodation, activity, package) via a local relative path and base64 embedding. Note: some files show encoding/BOM changes and minor whitespace/newline additions during the edit.

--- utils/receiptActivityPdfGenerator.js
+++ utils/receiptActivityPdfGenerator.js
@@ -6,7 +6,7 @@ const EXPLORE_SABAH_BASE64 = (() => {
   try {
     const imgPath = path.join(
       __dirname,
-      "../../rural-tourism-frontend/src/assets/icon/explore_sabah-without_bg.png",
+      "../assets/explore_sabah-without_bg.png",
     );
     return fs.readFileSync(imgPath).toString("base64");
   } catch {
